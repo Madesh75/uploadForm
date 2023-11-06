02b993e1a583e0bb1f42e27d4d06bf41c7750ed6
Madesh75 <119337609+Madesh75@users.noreply.github.com>
2023-11-06 19:16:10 +0530

    formData

--- src/app/excel-upload/excel-upload.component.ts
+++ src/app/excel-upload/excel-upload.component.ts
@@ -45,4 +45,60 @@ export class ExcelUploadComponent {
     }
   }
 }
+// import { Component } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+
+// @Component({
+//   selector: 'app-excel-upload',
+//   templateUrl: './excel-upload.component.html',
+// })
+// export class ExcelUploadComponent {
+//   formData: FormData = new FormData();
+//   errorMessage: string = '';
+//   uploadSuccessMessage: string = '';
+
+//   constructor(private http: HttpClient) {}
+
+//   fileChanged(event: any) {
+//     const selectedFile = event.target.files[0];
+
+//     if (!selectedFile) {
+//       this.errorMessage = 'No file selected. Please select a file.';
+//       this.uploadSuccessMessage = '';
+//       return;
+//     }
+
+//     if (selectedFile.type !== 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet') {
+//       this.errorMessage = 'Invalid file format. Please select an Excel file (XLSX format).';
+//       this.uploadSuccessMessage = '';
+//       return;
+//     }
+
+//     this.errorMessage = '';
+//     this.formData.delete('myfile'); // Clear any previous file
+//     this.formData.append('myfile', selectedFile);
+//   }
+
+//   uploadAndCheck() {
+//     if (!this.formData.has('myfile')) {
+//       this.errorMessage = 'No file selected. Please select a file.';
+//       this.uploadSuccessMessage = '';
+//       return;
+//     }
+
+//     this.errorMessage = '';
+//     this.uploadSuccessMessage = 'Uploading...';
+
+//     // Replace the URL with your server endpoint where you want to handle the file upload.
+//     this.http.post('http://your-server-endpoint.com/upload', this.formData).subscribe(
+//       (response) => {
+//         this.uploadSuccessMessage = 'File uploaded successfully.';
+//       },
+//       (error) => {
+//         this.errorMessage = 'File upload failed. Please try again.';
+//       }
+//     );
+//   }
+// }
+
 
